2ecd2ff0fe7b558b3300746fcb4a7df255c84818
GregRoss <GregRoss@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-16 00:14:13 +0000

    - Inital commit.
    git-svn-id: https://plugins.svn.wordpress.org/plugin-info-accordion/trunk@1161577 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ plugin-info-accordion.php
@@ -0,0 +1,55 @@
+<?php
+/*
+Plugin Name:  Plugin Info Accordion
+Description:  Adds an accordion to the FAQ and change log info from the Plugin Info plugin.
+Plugin URI:   http://toolstack.com/plugin-info-accordion
+Version:      0.1
+Author:       Greg Ross
+Author URI:   http://toolstack.com
+License:      GPL v2
+
+Compatible with WordPress 3+.
+
+Read the accompanying readme.txt file for instructions and documentation.
+
+Copyright (c) 2015 by Greg Ross
+
+This software is released under the GPL v2.0, see license.txt for details
+
+*/
+
+function plugin_info_accordion( $output, $attribute, $slug ) {
+	if ( 'faq' == $attribute || 'changelog' == $attribute ) {
+		// Add div tags before/after each of the h4's.
+		$output = str_replace( array( '<h4>', '</h4>' ), array( '</div><h4>', '</h4><div>' ), $output );
+		
+		// Get rid of the first closing div as we haven't opened a div yet.
+		$output = preg_replace('/\<\/div\>\<h4\>/', '<h4>', $output, 1); 
+		
+		// Close the last div we opened.
+		$output = $output . '</div>';
+
+		// Enqueue jquery ui components.
+		wp_enqueue_script('jquery-ui-core');
+		wp_enqueue_script('jquery-ui-accordion');
+		
+		// Load the jQuery UI CSS.
+		wp_register_style("pia_jquery-ui-css", plugin_dir_url(__FILE__) . "jquery-ui/jquery-ui.css");
+		wp_enqueue_style("pia_jquery-ui-css");
+
+		// Add a div around the entire output and give it an id so we can use it to activate the accordion.
+		$output = '<div id=' . $slug . '-' . $attribute . '>' . $output . '</div>';
+		
+		// Setup the default options for the accordion.
+		$options = 'animate: false, heightStyle : "content", collapsible: true'; 
+		
+		// By default the first item is visible, for FAQ's that probably doesn't make sense so close it.
+		if( 'faq' == $attribute ) { $options .= ', active: false'; }
+		
+		// Add the javascript to create the accordion.
+		$output = $output . '<script>jQuery(document).ready(function(){ jQuery("#' . $slug . '-' . $attribute . '").accordion( { ' . $options . ' } ); } );</script>';
+	}
+	
+	return $output;
+}
+add_filter( 'plugin_info_shortcode', 'plugin_info_accordion', 10, 3 );
---
+++ readme.txt
@@ -0,0 +1,46 @@
+=== Plugin Info Accordion ===
+Contributors: GregRoss
+Tags:  plugin, info, data, utility, developer, meta, tool
+Requires at least: 3.4
+Tested up to: 4.2
+Author URI: http://toolstack.com
+Plugin URI: http://toolstack.com/plugin-info-accordion
+Stable tag: 1.0
+License: GPLv2
+
+Adds an accordion to the FAQ and change log info from the Plugin Info plugin.
+
+== Description ==
+
+This plugin is an extension to [Plugin Info](https://wordpress.org/plugins/plugin-info/), you must have Plugin Information installed to use it!
+
+Once installed an activated, the "[plugin faq]" and "[plugin changelog]" shortcodes will be displayed with a jQueryUI Accordion control.
+
+There are no options for this plugin, hence no settings page.
+
+== Installation ==
+
+1. Extract the archive file into your plugins directory in the plugin-info-accordion folder.
+2. Activate the plugin in the Plugin options.
+
+== Frequently Asked Questions ==
+
+= None at this time =
+
+This is where the FAQ will go.
+
+== Screenshots ==
+
+1. FAQ accordion.
+2. Change log accordion.
+
+== Changelog ==
+= 1.0 =
+* Release date: May 15, 2015
+* Initial release.
+
+== Upgrade Notice ==
+
+= 1.0 =
+None.
+
